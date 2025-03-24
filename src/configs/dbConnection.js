99a9bb1ed22a8885c95bcc03350e1cbd4f12089f
haakonerol <haakon.erol@gmail.com>
2025-03-24 02:54:40 +0100

    connected cloud

--- src/configs/dbConnection.js
+++ src/configs/dbConnection.js
@@ -8,7 +8,7 @@ const mongoose = require("mongoose");
 
 const dbConnection = () => {
   mongoose
-    .connect(process.env.MONGODB)
+    .connect(process.env.MONGODBCLOUD)
     .then(() => console.log("* DB Connected *"))
     .catch((err) => console.log("* DB Not Connected *", err));
 };
